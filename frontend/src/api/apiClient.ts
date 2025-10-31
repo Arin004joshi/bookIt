8f95707e41ae1f88ac1b555a97a263ff0534068e
Arin <arinkumarjoshi@gmail.com>
2025-10-31 18:17:51 +0530

    configuring the hosted backend url

--- frontend/src/api/apiClient.ts
+++ frontend/src/api/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Set your backend URL here. You may want to use an environment variable (VITE_API_BASE_URL)
-const API_BASE_URL = 'http://localhost:5000/api/v1';
+const API_BASE_URL = 'https://bookit-etlw.onrender.com';
 
 const apiClient = axios.create({
     baseURL: API_BASE_URL,
